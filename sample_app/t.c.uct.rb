ab59ceda6f7f7da4d7152a5eeb3184738aed7d4a
ibtechraj <raj_shk@rediffmail.com>
2019-07-13 11:42:10 +0530

    Finished ch 10 with test error

--- sample_app/t.c.uct.rb
+++ sample_app/t.c.uct.rb
@@ -12,12 +12,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-   test "should get new" do
-    get signup_path
-    assert_response :success
-  end
-#end
-
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
     assert_not flash.empty?
@@ -60,4 +54,5 @@ test "should redirect destroy when not logged in" do
     end
     assert_redirected_to root_url
   end
+
 end
